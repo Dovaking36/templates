a9e1d841317c840c40e1d44bf7d6a2cb3c7b9038
egoro <egorovvdm@gmail.com>
2026-07-01 13:00:41 +1100

    1

--- test.py
+++ test.py
@@ -1,1 +1,19 @@
+class Bolt:
+    def __init__(self, length,rezba,shape_of_head):
+        self.length = length
+        self.rezba = rezba
+        self.shape = shape_of_head
+        
+    def spin(self):
+        print(f"БОЛТ {self.shape} БЫЛ ЗАКРУЧЕН")
+        
+    def __str__(self) -> str:
+        return f"Форма: {self.shape}\nДлина: {self.length}\nРезьба: {self.rezba}"
+    
+        
+        
+b1 = Bolt(12,"прямая","+")
+b2 = Bolt(14,"обратная","-")
 
+print(b1)
+print(b2)
